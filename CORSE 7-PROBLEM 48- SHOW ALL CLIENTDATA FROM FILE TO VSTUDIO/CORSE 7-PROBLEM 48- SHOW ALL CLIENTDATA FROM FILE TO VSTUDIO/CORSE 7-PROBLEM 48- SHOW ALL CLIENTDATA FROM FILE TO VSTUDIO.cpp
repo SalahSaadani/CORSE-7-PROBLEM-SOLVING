c1e380014a4684dd8f4ff83a837ec45164791d37
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-04-23 08:58:04 +0200

    Add files via upload

---
+++ CORSE 7-PROBLEM 48- SHOW ALL CLIENTDATA FROM FILE TO VSTUDIO/CORSE 7-PROBLEM 48- SHOW ALL CLIENTDATA FROM FILE TO VSTUDIO/CORSE 7-PROBLEM 48- SHOW ALL CLIENTDATA FROM FILE TO VSTUDIO.cpp
@@ -0,0 +1,128 @@
+// CORSE 7-PROBLEM 48- SHOW ALL CLIENTDATA FROM FILE TO VSTUDIO.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+struct sClient
+{ 
+    string AccountNumber;
+    string PinCode;
+    string Name; 
+    string Phone; 
+    double AccountBalance;
+};
+vector<string> SplitString(string S1, string Delim)
+{
+    vector<string> vString;
+    short pos = 0;
+    string sWord; // define a string variable
+                  
+   while ((pos = S1.find(Delim)) != std::string::npos)  // use find() function to get the position of the delimiters
+   {  
+       sWord = S1.substr(0, pos); // store the word  
+       if (sWord != "")   
+       {  
+           vString.push_back(sWord);
+       }
+       S1.erase(0, pos + Delim.length()); 
+   } 
+   if (S1 != "")  
+   {    
+       vString.push_back(S1); // it adds last word of the string. 
+   }
+   return vString;
+} 
+sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
+{
+    sClient Client;
+    vector<string> vClientData; 
+
+    vClientData = SplitString(Line, Seperator);
+
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];  
+    Client.Name = vClientData[2]; 
+    Client.Phone = vClientData[3];  
+    Client.AccountBalance = stod(vClientData[4]);//cast string to double
+
+    return Client;
+}
+vector <sClient> LoadCleintsDataFromFile(string FileName)
+{
+    vector <sClient> vClients; 
+    fstream MyFile;
+
+    MyFile.open(FileName, ios::in);//read Mode
+
+    if (MyFile.is_open()) 
+    {
+        string Line;
+        sClient Client;
+
+        while (getline(MyFile, Line))  
+        { 
+            Client = ConvertLinetoRecord(Line);
+            vClients.push_back(Client);    
+        } 
+
+        MyFile.close();
+    }
+
+    return vClients;
+} 
+void PrintClientRecord(sClient Client)
+{
+    cout << "| " << setw(15) << left << Client.AccountNumber; 
+    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(40) << left << Client.Name; 
+    cout << "| " << setw(12) << left << Client.Phone; 
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+}
+void PrintAllClientsData(vector <sClient> vClients)
+{ 
+    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s)."; 
+    cout << "\n_______________________________________________________"; 
+    cout << "_________________________________________\n" << endl;
+    cout << "| " << left << setw(15) << "Accout Number";   
+    cout << "| " << left << setw(10) << "Pin Code";   
+    cout << "| " << left << setw(40) << "Client Name"; 
+    cout << "| " << left << setw(12) << "Phone";   
+    cout << "| " << left << setw(12) << "Balance";  
+    cout << "\n_______________________________________________________";  
+    cout << "_________________________________________\n" << endl;
+    
+    for (sClient Client : vClients)
+    { 
+        PrintClientRecord(Client);   
+        cout << endl;
+    }  
+    cout << "\n_______________________________________________________"; 
+    cout << "_________________________________________\n" << endl;
+}
+
+int main()
+{
+    vector <sClient> vClients = LoadCleintsDataFromFile(ClientsFileName);
+
+    PrintAllClientsData(vClients);
+
+    system("pause>0");
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
